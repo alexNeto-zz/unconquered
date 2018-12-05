69d06dc3ba2acd0c0182c5debaee56d3e3fbd21a
alexNeto <alex.neto3@hotmail.com>
2018-12-04 22:13:03 -0200

    done setup

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+          <h1>aaaaa</h1>
+        </header>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Header;
